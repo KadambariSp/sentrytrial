a97e7c52f35e2889340280d3fa4681ef06d54e8b
KadambariSp <k19patangrao@gmail.com>
2022-02-02 16:16:18 +0530

    sentry trial

--- src/index.js
+++ src/index.js
@@ -2,8 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
 import reportWebVitals from './reportWebVitals';
 
+Sentry.init({
+  dsn: "https://2ef3ad996677482b820a4cbbd8902c6d@o1128175.ingest.sentry.io/6179695",
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 ReactDOM.render(
   <React.StrictMode>
     <App />
